5edd3adfa201f758e0bd343b9c7612815e3042df
Carl Parm <carlparm@gmail.com>
2020-01-16 16:43:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,26 +4,32 @@ def third_challenge
       :patriarch => {
         :name => "Lorf Montague",
         :age => "53",
-        
-        
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        :B]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
