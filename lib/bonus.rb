7536fdeef6d69e5bfbf1d0f08de19f593eff9a67
Carl Parm <carlparm@gmail.com>
2020-01-16 17:00:17 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,13 +22,8 @@ def bonus
 
 epic_tragedy[:montague][:hero][:status] = "dead"
 
-puts epic_tragedy
-
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-puts epic_tragedy
-
-return epic_tragedy
 
   
 
