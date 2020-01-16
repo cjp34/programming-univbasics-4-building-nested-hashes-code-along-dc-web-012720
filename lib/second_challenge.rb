85ab87327acfc4b56de84778d32144da81b448ab
Carl Parm <carlparm@gmail.com>
2020-01-16 16:35:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,13 +2,13 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matricarh => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {},
-      :matricarh => {},
+      :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }
